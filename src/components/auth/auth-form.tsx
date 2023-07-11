fbaf555ade30f60b0bbf8e1910eba46ebac756ff
Sekkacchi <sekkacchi@gmail.com>
2023-07-11 17:18:27 -0400

    Implement types on auth form and input component.

--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -4,8 +4,18 @@ import * as Types from "@/types";
 import Styles from "./auth-form.module.scss";
 
 export const AuthForm = (props: Types.AuthFormProps) => {
+  function submit(event: Types.OnSubmit) {
+    event.preventDefault();
+
+    if (props.type === "Register") {
+      alert("Submitted Registration Form!");
+    } else {
+      alert("Submitted Login Form!");
+    }
+  }
+
   return (
-    <form className={Styles.form}>
+    <form className={Styles.form} onSubmit={submit}>
       <AuthHeader type={props.type} />
 
       <div className={Styles.body}></div>
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,13 +1,19 @@
+import * as Types from "@/types";
+
 import Styles from "./input.module.scss";
 
 type Props = {
   value: string;
   placeholder: string;
-  setValue: () => void;
+  setValue: (event: Types.OnChange) => void;
 };
 
 export const Input = (props: Props) => {
   <article className={Styles.article}>
-    <input value={props.value} placeholder={props.placeholder} />
+    <input
+      onChange={props.setValue}
+      value={props.value}
+      placeholder={props.placeholder}
+    />
   </article>;
 };
