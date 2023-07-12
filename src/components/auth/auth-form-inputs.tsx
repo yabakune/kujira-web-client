4f34cf308622b4514628f5f687c13354459b9a05
Sekkacchi <sekkacchi@gmail.com>
2023-07-12 12:12:13 -0400

    Refactor auth form inputs into their own component.

---
+++ src/components/auth/auth-form-inputs.tsx
@@ -0,0 +1,153 @@
+import { Signal, effect } from "@preact/signals-react";
+
+import * as Components from "@/components";
+import * as Types from "@/types";
+
+import Styles from "./auth-form-inputs.module.scss";
+
+type Props = {
+  email: Signal<string>;
+  username: Signal<string>;
+  password: Signal<string>;
+  confirmPassword: Signal<string>;
+  emailError: Signal<string>;
+  usernameError: Signal<string>;
+  passwordError: Signal<string>;
+  confirmPasswordError: Signal<string>;
+} & Types.AuthFormProps;
+
+function checkValidUsername(username: string): boolean {
+  return /^[a-zA-Z0-9](?:[a-zA-Z0-9_.-]*[a-zA-Z0-9])?$/.test(username);
+}
+
+function checkValidPassword(password: string): boolean {
+  return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@%#]).*$/.test(password);
+}
+
+export const AuthFormInputs = (props: Props) => {
+  function handleEmailErrors(): void {
+    if (props.email.value.length === 0) {
+      props.emailError.value = "";
+    } else {
+      if (!props.email.value.includes("@")) {
+        props.emailError.value = 'Please include "@"';
+      } else if (!props.email.value.includes(".com")) {
+        props.emailError.value = 'Please include ".com"';
+      } else {
+        props.emailError.value = "";
+      }
+    }
+  }
+
+  function handleUsernameErrors(): void {
+    if (props.username.value.length === 0) {
+      props.usernameError.value = "";
+    } else {
+      if (!checkValidUsername(props.username.value)) {
+        props.usernameError.value = "Invalid username.";
+      } else if (props.username.value.length > 14) {
+        props.usernameError.value = "Username too long.";
+      } else {
+        props.usernameError.value = "";
+      }
+    }
+  }
+
+  function handlePasswordErrors(): void {
+    if (props.password.value.length === 0) {
+      props.passwordError.value = "";
+    } else {
+      if (!checkValidPassword(props.password.value)) {
+        props.passwordError.value =
+          "Password must contain at least one capital and lowercase letter and at least one of the following special characters: @, !, %, #.";
+      } else if (props.password.value.length < 12) {
+        props.passwordError.value = "Password too short.";
+      } else {
+        props.passwordError.value = "";
+      }
+    }
+  }
+
+  function handleConfirmPasswordErrors(): void {
+    if (props.confirmPassword.value.length === 0) {
+      props.confirmPasswordError.value = "";
+    } else {
+      if (props.confirmPassword.value !== props.password.value) {
+        props.confirmPasswordError.value = "Passwords must match.";
+      } else {
+        props.confirmPasswordError.value = "";
+      }
+    }
+  }
+
+  effect(() => {
+    handleEmailErrors();
+    if (props.type === "Register") {
+      handleUsernameErrors();
+      handlePasswordErrors();
+      handleConfirmPasswordErrors();
+    }
+  });
+
+  return (
+    <article className={Styles.inputs}>
+      {props.emailError.value && (
+        <span className={Styles.error}>{props.emailError.value}</span>
+      )}
+      <Components.Input
+        key="Auth Form Email Input"
+        type="email"
+        userInput={props.email}
+        placeholder="Email"
+        backgroundLevel={2}
+        required
+      />
+
+      {props.type === "Register" && (
+        <>
+          {props.usernameError.value && (
+            <span className={Styles.error}>{props.usernameError.value}</span>
+          )}
+          <Components.Input
+            key="Auth Form Username Input"
+            type="text"
+            userInput={props.username}
+            placeholder="Username"
+            backgroundLevel={2}
+            required
+          />
+        </>
+      )}
+
+      {props.passwordError.value && (
+        <span className={Styles.error}>{props.passwordError.value}</span>
+      )}
+      <Components.Input
+        key="Auth Form Password Input"
+        type="password"
+        userInput={props.password}
+        placeholder="Password"
+        backgroundLevel={2}
+        required
+      />
+
+      {props.type === "Register" && (
+        <>
+          {props.confirmPasswordError.value && (
+            <span className={Styles.error}>
+              {props.confirmPasswordError.value}
+            </span>
+          )}
+          <Components.Input
+            key="Auth Form Confirm Password Input"
+            type="password"
+            userInput={props.confirmPassword}
+            placeholder="Confirm Password"
+            backgroundLevel={2}
+            required
+          />
+        </>
+      )}
+    </article>
+  );
+};
--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -1,4 +1,4 @@
-import { effect, useSignal } from "@preact/signals-react";
+import { useSignal } from "@preact/signals-react";
 
 import * as Components from "@/components";
 import * as Types from "@/types";
@@ -7,14 +7,7 @@ import { AuthFormHeader } from "./auth-form-header";
 import { AuthFormAgreement } from "./auth-form-agreement";
 
 import Styles from "./auth-form.module.scss";
-
-function checkValidUsername(username: string): boolean {
-  return /^[a-zA-Z0-9](?:[a-zA-Z0-9_.-]*[a-zA-Z0-9])?$/.test(username);
-}
-
-function checkValidPassword(password: string): boolean {
-  return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@%#]).*$/.test(password);
-}
+import { AuthFormInputs } from "./auth-form-inputs";
 
 export const AuthForm = (props: Types.AuthFormProps) => {
   const email = useSignal("");
@@ -28,70 +21,6 @@ export const AuthForm = (props: Types.AuthFormProps) => {
   const usernameError = useSignal("");
   const passwordError = useSignal("");
   const confirmPasswordError = useSignal("");
-
-  function handleEmailErrors(): void {
-    if (email.value.length === 0) {
-      emailError.value = "";
-    } else {
-      if (!email.value.includes("@")) {
-        emailError.value = 'Please include "@"';
-      } else if (!email.value.includes(".com")) {
-        emailError.value = 'Please include ".com"';
-      } else {
-        emailError.value = "";
-      }
-    }
-  }
-
-  function handleUsernameErrors(): void {
-    if (username.value.length === 0) {
-      usernameError.value = "";
-    } else {
-      if (!checkValidUsername(username.value)) {
-        usernameError.value = "Invalid username.";
-      } else if (username.value.length > 14) {
-        usernameError.value = "Username too long.";
-      } else {
-        usernameError.value = "";
-      }
-    }
-  }
-
-  function handlePasswordErrors(): void {
-    if (password.value.length === 0) {
-      passwordError.value = "";
-    } else {
-      if (!checkValidPassword(password.value)) {
-        passwordError.value =
-          "Password must contain at least one capital and lowercase letter and at least one of the following special characters: @, !, %, #.";
-      } else if (password.value.length < 12) {
-        passwordError.value = "Password too short.";
-      } else {
-        passwordError.value = "";
-      }
-    }
-  }
-
-  function handleConfirmPasswordErrors(): void {
-    if (confirmPassword.value.length === 0) {
-      confirmPasswordError.value = "";
-    } else {
-      if (confirmPassword.value !== password.value) {
-        confirmPasswordError.value = "Passwords must match.";
-      } else {
-        confirmPasswordError.value = "";
-      }
-    }
-  }
-
-  effect(() => {
-    handleEmailErrors();
-    if (props.type === "Register") {
-      handleUsernameErrors();
-      handlePasswordErrors();
-      handleConfirmPasswordErrors();
-    }
-  });
 
   function handleButtonDisable(): boolean {
     if (props.type === "Register") {
@@ -130,65 +59,17 @@ export const AuthForm = (props: Types.AuthFormProps) => {
       <section className={Styles.body}>
         <AuthFormHeader type={props.type} />
 
-        <article className={Styles.inputs}>
-          {emailError.value && (
-            <span className={Styles.error}>{emailError.value}</span>
-          )}
-          <Components.Input
-            key="Auth Form Email Input"
-            type="email"
-            userInput={email}
-            placeholder="Email"
-            backgroundLevel={2}
-            required
-          />
-
-          {props.type === "Register" && (
-            <>
-              {usernameError.value && (
-                <span className={Styles.error}>{usernameError.value}</span>
-              )}
-              <Components.Input
-                key="Auth Form Username Input"
-                type="text"
-                userInput={username}
-                placeholder="Username"
-                backgroundLevel={2}
-                required
-              />
-            </>
-          )}
-
-          {passwordError.value && (
-            <span className={Styles.error}>{passwordError.value}</span>
-          )}
-          <Components.Input
-            key="Auth Form Password Input"
-            type="password"
-            userInput={password}
-            placeholder="Password"
-            backgroundLevel={2}
-            required
-          />
-
-          {props.type === "Register" && (
-            <>
-              {confirmPasswordError.value && (
-                <span className={Styles.error}>
-                  {confirmPasswordError.value}
-                </span>
-              )}
-              <Components.Input
-                key="Auth Form Confirm Password Input"
-                type="password"
-                userInput={confirmPassword}
-                placeholder="Confirm Password"
-                backgroundLevel={2}
-                required
-              />
-            </>
-          )}
-        </article>
+        <AuthFormInputs
+          email={email}
+          username={username}
+          password={password}
+          confirmPassword={confirmPassword}
+          emailError={emailError}
+          usernameError={usernameError}
+          passwordError={passwordError}
+          confirmPasswordError={confirmPasswordError}
+          type={props.type}
+        />
 
         <AuthFormAgreement
           type={props.type}
