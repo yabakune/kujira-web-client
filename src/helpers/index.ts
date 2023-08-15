3550d8d135eff5dd263afafa7f9f100a5975f501
Sekkacchi <sekkacchi@gmail.com>
2023-08-15 08:39:09 -0400

    Remove non-existing imports in helpers index.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,5 @@ export * from "./styles.helpers";
 export * from "./forms.helpers";
 export * from "./sagas.helpers";
 export * from "./metadata.helpers";
-export * from "./strings.helpers";
 export * from "./dates.helpers";
-export * from "./maths.helpers";
 export * from "./performance.helpers";
