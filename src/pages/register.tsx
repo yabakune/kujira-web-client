35d101338e86df9ed2495e255bbc2d6582b2b203
yabakune <putecchi@gmail.com>
2023-08-15 10:35:30 -0400

    Create registration page.

---
+++ src/pages/register.tsx
@@ -0,0 +1,11 @@
+import * as Components from "@/components";
+
+export default function Register() {
+  return (
+    <>
+      <Components.PageHead title="Register" />
+
+      <main>Registration Page</main>
+    </>
+  );
+}
