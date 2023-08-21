cd00e7c91162e51603099574cb56b80051971bf6
yabakune <putecchi@gmail.com>
2023-08-21 07:44:42 -0400

    Remove all old gated/ungated routes logic in app layout.

--- src/components/layouts/app-layout.tsx
+++ src/components/layouts/app-layout.tsx
@@ -21,22 +21,8 @@ type Props = { children: React.ReactNode };
 
 const mulish = Mulish({ subsets: ["latin"] });
 
-const gatedRoutes: string[] = [
-  Constants.ClientRoutes.LOGBOOKS,
-  Constants.ClientRoutes.REVIEWS,
-  Constants.ClientRoutes.SETTINGS,
-  Constants.ClientRoutes.BUG_REPORT,
-];
-
-const ungatedRoutes: string[] = [
-  Constants.ClientRoutes.LANDING,
-  Constants.ClientRoutes.REGISTER,
-  Constants.ClientRoutes.LOGIN,
-];
-
 export const AppLayout = (props: Props) => {
   const dispatch = useDispatch();
-  const router = useRouter();
   const { currentUser } = useSelector((state: ReduxStore) => state.entities);
 
   useEffect(() => {
@@ -45,50 +31,36 @@ export const AppLayout = (props: Props) => {
         dispatch(Sagas.fetchUserRequest({ userId: Constants.userId }));
       }
     }
+
+    // if (typeof window !== "undefined") {
+    //   const localStorageTheme = localStorage.getItem("theme");
+
+    //   if (currentUser.theme && localStorageTheme !== currentUser.theme) {
+    //     localStorage.setItem("theme", currentUser.theme);
+    //   }
+
+    //   if (currentUser.theme === "dark") {
+    //     document.body.classList.remove(ThemeStyles.themeLilac);
+    //     document.body.classList.remove(ThemeStyles.systemTheme);
+    //   } else if (currentUser.theme === "light") {
+    //     document.body.classList.remove(ThemeStyles.systemTheme);
+    //     document.body.classList.add(ThemeStyles.themeLilac);
+    //   } else if (currentUser.theme === "system") {
+    //     document.body.classList.remove(ThemeStyles.themeLilac);
+    //     document.body.classList.add(ThemeStyles.systemTheme);
+    //   } else {
+    //     const date = new Date();
+    //     const beforeSixPM = date.getHours() < 18;
+    //     if (beforeSixPM) {
+    //       document.body.classList.remove(ThemeStyles.systemTheme);
+    //       document.body.classList.add(ThemeStyles.themeLilac);
+    //     } else {
+    //       document.body.classList.remove(ThemeStyles.themeLilac);
+    //       document.body.classList.remove(ThemeStyles.systemTheme);
+    //     }
+    //   }
+    // }
   }, []);
-
-  // useEffect(() => {
-  //   if (Constants.userId && !currentUser) {
-  //     dispatch(Sagas.fetchUserRequest({ userId: Constants.userId }));
-  //   } else if (!currentUser) {
-  //     if (gatedRoutes.includes(router.pathname)) {
-  //       router.push(Constants.ClientRoutes.LANDING);
-  //     }
-  //   } else {
-  //     if (ungatedRoutes.includes(router.pathname)) {
-  //       router.push(Constants.ClientRoutes.LOGBOOKS);
-  //     }
-
-  //     if (typeof window !== "undefined") {
-  //       const localStorageTheme = localStorage.getItem("theme");
-
-  //       if (currentUser.theme && localStorageTheme !== currentUser.theme) {
-  //         localStorage.setItem("theme", currentUser.theme);
-  //       }
-
-  //       if (currentUser.theme === "dark") {
-  //         document.body.classList.remove(ThemeStyles.themeLilac);
-  //         document.body.classList.remove(ThemeStyles.systemTheme);
-  //       } else if (currentUser.theme === "light") {
-  //         document.body.classList.remove(ThemeStyles.systemTheme);
-  //         document.body.classList.add(ThemeStyles.themeLilac);
-  //       } else if (currentUser.theme === "system") {
-  //         document.body.classList.remove(ThemeStyles.themeLilac);
-  //         document.body.classList.add(ThemeStyles.systemTheme);
-  //       } else {
-  //         const date = new Date();
-  //         const beforeSixPM = date.getHours() < 18;
-  //         if (beforeSixPM) {
-  //           document.body.classList.remove(ThemeStyles.systemTheme);
-  //           document.body.classList.add(ThemeStyles.themeLilac);
-  //         } else {
-  //           document.body.classList.remove(ThemeStyles.themeLilac);
-  //           document.body.classList.remove(ThemeStyles.systemTheme);
-  //         }
-  //       }
-  //     }
-  //   }
-  // }, [currentUser]);
 
   return (
     <div className={mulish.className}>
