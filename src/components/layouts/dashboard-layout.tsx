091dad2e3cf46a8c07330898a85a6f76b8dcfce1
yabakune <putecchi@gmail.com>
2023-08-21 08:10:31 -0400

    Onboarding and dashboard pages redirect user to login page if they're not logged in.

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -1,7 +1,29 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import * as Constants from "@/constants";
+import * as Redux from "@/redux";
+
 type Props = {
   children: React.ReactNode;
 };
 
 export const DashboardLayout = (props: Props) => {
+  const router = useRouter();
+
+  const { ClientRoutes } = Constants;
+  const { currentUser } = useSelector(
+    (state: Redux.ReduxStore) => state.entities
+  );
+
+  useEffect(() => {
+    if (currentUser) {
+      if (!currentUser.onboarded) router.push(ClientRoutes.ONBOARDING);
+    } else {
+      router.push(ClientRoutes.LOGIN);
+    }
+  }, [currentUser]);
+
   return <>Dashboard Layout {props.children}</>;
 };
--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,13 +1,29 @@
-import * as Components from "@/components";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import * as Constants from "@/constants";
+import * as Redux from "@/redux";
 
 import { NextPageWithLayout } from "./_app";
+import { useRouter } from "next/router";
 
 const Onboarding: NextPageWithLayout = () => {
-  return <div>Onboarding</div>;
-};
+  const router = useRouter();
 
-Onboarding.getLayout = function getLayout(page: React.ReactElement) {
-  return <Components.DashboardLayout>{page}</Components.DashboardLayout>;
+  const { ClientRoutes } = Constants;
+  const { currentUser } = useSelector(
+    (state: Redux.ReduxStore) => state.entities
+  );
+
+  useEffect(() => {
+    if (currentUser) {
+      if (currentUser.onboarded) router.push(ClientRoutes.LOGBOOKS);
+    } else {
+      router.push(ClientRoutes.LOGIN);
+    }
+  }, [currentUser]);
+
+  return <div>Onboarding</div>;
 };
 
 export default Onboarding;
