c8f45b5d81afb2b79ed8ad3177717ae2f7ab7087
yabakune <putecchi@gmail.com>
2023-08-21 09:22:19 -0400

    Add submit button and submit button text logic to onboarding page.

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,4 +1,4 @@
-import { useSignal } from "@preact/signals-react";
+import { effect, useSignal } from "@preact/signals-react";
 import { useRouter } from "next/router";
 import { useCallback, useEffect } from "react";
 import { useSelector } from "react-redux";
@@ -21,6 +21,23 @@ const Onboarding: NextPageWithLayout = () => {
   );
 
   const page = useSignal(1);
+  const buttonText = useSignal("Let's go!");
+
+  effect(() => {
+    if (page.value === 1) {
+      buttonText.value = "Let's go!";
+    } else if (page.value === 2) {
+      buttonText.value = "Savings";
+    } else if (page.value === 3) {
+      buttonText.value = "Recurring Purchases";
+    } else if (page.value === 4) {
+      buttonText.value = "Incoming Purchases";
+    } else if (page.value === 5) {
+      buttonText.value = "Final Step";
+    } else {
+      buttonText.value = "I'm ready.";
+    }
+  });
 
   function incrementPage(): void {
     if (page.value < 6) page.value += 1;
@@ -64,6 +81,14 @@ const Onboarding: NextPageWithLayout = () => {
         ) : (
           <></>
         )}
+
+        <Components.Button
+          text={buttonText.value}
+          rightIcon={<Components.ArrowRight width={14} fill={12} />}
+          centered
+          primary
+          submit
+        />
       </form>
     </main>
   );
