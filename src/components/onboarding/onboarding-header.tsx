fb33fa13058a5e103f031b4bb20bb4cd449b6a59
yabakune <putecchi@gmail.com>
2023-08-21 09:27:10 -0400

    <OnboardingHeader /> now only shows decrement arrow when greater than first page.

--- src/components/onboarding/onboarding-header.tsx
+++ src/components/onboarding/onboarding-header.tsx
@@ -14,15 +14,14 @@ type Props = {
 export const OnboardingHeader = (props: Props) => {
   return (
     <header className={Styles.container}>
-      <button
-        className={Snippets.iconButton}
-        type="button"
-        onClick={props.decrementPage}
-      ></button>
-
-      <Components.ButtonIcon backgroundLevel={2} onClick={props.decrementPage}>
-        <Components.ArrowLeft width={14} fill={8} />
-      </Components.ButtonIcon>
+      {props.page.value > 1 && (
+        <Components.ButtonIcon
+          backgroundLevel={2}
+          onClick={props.decrementPage}
+        >
+          <Components.ArrowLeft width={14} fill={8} />
+        </Components.ButtonIcon>
+      )}
 
       <h1 className={`${Styles.title} ${Snippets.titleText}`}>{props.title}</h1>
       <h2 className={Styles.page}>{props.page.value}/6</h2>
--- src/components/onboarding/onboarding-welcome.tsx
+++ src/components/onboarding/onboarding-welcome.tsx
@@ -3,10 +3,25 @@ import * as Types from "@/types";
 
 export const OnboardingWelcome = (props: Types.OnboardingPage) => {
   return (
-    <Components.OnboardingHeader
-      title="Hi, and welcome to Kujira!"
-      page={props.page}
-      decrementPage={props.decrementPage}
-    />
+    <>
+      <Components.OnboardingHeader
+        title="Hi, and welcome to Kujira!"
+        page={props.page}
+        decrementPage={props.decrementPage}
+      />
+
+      <p>
+        This app was created to help you manage your financial health on a
+        monthly basis. In order to achieve that, however, you’re going to have
+        to fill in some important information in the following steps with as
+        much accuracy as possible. Please do not round any values.
+      </p>
+
+      <p>
+        Rest assured, all information you provide in the next few steps is
+        private to you and will never be shared with anyone else. With that
+        said, let’s do some setup, starting with your take-home income!
+      </p>
+    </>
   );
 };
