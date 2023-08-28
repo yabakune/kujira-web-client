76ee4a5fe580fbb3f19b921b0620bd12214497f0
Sekkacchi <sekkacchi@gmail.com>
2023-08-28 08:20:54 -0400

    Create base for dashboard layout and dashboard settings page.

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -1,39 +1,11 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
-
-import * as Constants from "@/constants";
-import * as Helpers from "@/helpers";
-import * as Redux from "@/redux";
-import * as Sagas from "@/sagas";
+import Styles from "./dashboard-layout.module.scss";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const DashboardLayout = (props: Props) => {
-  const dispatch = useDispatch();
-  const router = useRouter();
-
-  const { ClientRoutes } = Constants;
-  const { currentUser, logbooks } = useSelector(
-    (state: Redux.ReduxStore) => state.entities,
-    shallowEqual
+  return (
+    <div className={Styles.container}>Dashboard Layout {props.children}</div>
   );
-
-  // useEffect(() => {
-  //   if (currentUser && !currentUser.onboarded) {
-  //     router.push(ClientRoutes.ONBOARDING);
-  //   } else {
-  //     router.push(ClientRoutes.LOGIN);
-  //   }
-  // }, [currentUser]);
-
-  // useEffect(() => {
-  //   if (!logbooks && Helpers.userId) {
-  //     dispatch(Sagas.fetchUserLogbooksRequest({ userId: Helpers.userId }));
-  //   }
-  // }, [logbooks]);
-
-  return <>Dashboard Layout {props.children}</>;
 };
---
+++ src/pages/dashboard/settings.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from "react";
+
+import * as Components from "@/components";
+import { NextPageWithLayout } from "../_app";
+
+const Settings: NextPageWithLayout = () => {
+  return (
+    <>
+      <Components.PageHead title="Settings" />
+      Settings
+    </>
+  );
+};
+
+Settings.getLayout = function getLayout(page: ReactElement) {
+  return <Components.DashboardLayout>{page}</Components.DashboardLayout>;
+};
+
+export default Settings;
