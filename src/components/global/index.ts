2be71df9ca32da3dfde810b3055fb5aec4614eb2
Sekkacchi <sekkacchi@gmail.com>
2023-08-28 08:03:07 -0400

    Move all global components into new "components/global" directory.

---
+++ src/components/global/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./button-icon";
+export * from "./input";
+export * from "./notification";
+export * from "./page-head";
+export * from "./progress-bar";
+export * from "./spacer";
