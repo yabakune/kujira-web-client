93af30b53d8327399bebbf0d68cb44165e78b126
Sekkacchi <sekkacchi@gmail.com>
2023-08-29 07:42:40 -0400

    Onboarding page redirects to logbooks page or login page.

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -32,7 +32,7 @@ function findEntryId(
 const Onboarding = () => {
   const dispatch = useDispatch();
 
-  const { logbooks, overviews, entries } = useSelector(
+  const { currentUser, logbooks, overviews, entries } = useSelector(
     (state: Redux.ReduxStore) => state.entities,
     shallowEqual
   );
@@ -171,40 +171,47 @@ const Onboarding = () => {
     }
   }
 
-  return (
-    <main className={Styles.container}>
-      <form
-        className={`${Styles.form} ${Helpers.setBackgroundLevel(2)}`}
-        onSubmit={nextPage}
-      >
-        <Components.OnboardingHeader page={page} />
+  if (currentUser) {
+    if (currentUser.onboarded) return <Components.ToLogbooks />;
+    else {
+      return (
+        <main className={Styles.container}>
+          <form
+            className={`${Styles.form} ${Helpers.setBackgroundLevel(2)}`}
+            onSubmit={nextPage}
+          >
+            <Components.OnboardingHeader page={page} />
 
-        {page.value > 1 && Number(income.value) >= 0 && (
-          <p className={Styles.highlight}>
-            ${Helpers.formatRoundedCost(remainingBudget.value)} remaining
-          </p>
-        )}
+            {page.value > 1 && Number(income.value) >= 0 && (
+              <p className={Styles.highlight}>
+                ${Helpers.formatRoundedCost(remainingBudget.value)} remaining
+              </p>
+            )}
 
-        <Components.OnboardingPages
-          page={page}
-          income={income}
-          savings={savings}
-          recurringPurchases={recurringPurchases}
-          incomingPurchases={incomingPurchases}
-          disabled={disabled}
-        />
+            <Components.OnboardingPages
+              page={page}
+              income={income}
+              savings={savings}
+              recurringPurchases={recurringPurchases}
+              incomingPurchases={incomingPurchases}
+              disabled={disabled}
+            />
 
-        <Components.Button
-          text={buttonText.value}
-          rightIcon={<Components.ArrowRight width={14} fill={12} />}
-          disabled={disabled}
-          centered
-          primary
-          submit
-        />
-      </form>
-    </main>
-  );
+            <Components.Button
+              text={buttonText.value}
+              rightIcon={<Components.ArrowRight width={14} fill={12} />}
+              disabled={disabled}
+              centered
+              primary
+              submit
+            />
+          </form>
+        </main>
+      );
+    }
+  } else {
+    return <Components.ToLogin />;
+  }
 };
 
 export default Onboarding;
