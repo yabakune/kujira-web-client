f949d4bc64e0765cd4f7765df5f2dcc6ebf62401
Sekkacchi <sekkacchi@gmail.com>
2023-10-02 13:03:36 -0400

    Home page is now effectively the login page; remove redirect.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,38 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
 
+// import * as Components from "@/components";
+// import * as Constants from "@/constants";
+
+// import styles from "@/styles/home.module.scss";
+
+import { ReactElement } from "react";
 import * as Components from "@/components";
-import * as Constants from "@/constants";
-
-import styles from "@/styles/home.module.scss";
+import Login from "./login";
 
 export default function Home() {
-  const router = useRouter();
+  return <Login />;
 
-  useEffect(() => {
-    router.replace(Constants.ClientRoutes.LOGIN, undefined, { shallow: true });
-  }, []);
+  // const router = useRouter();
 
+  // useEffect(() => {
+  //   router.replace(Constants.ClientRoutes.LOGIN, undefined, { shallow: true });
+  // }, []);
+
+  // return (
+  //   <>
+  //     <Components.PageHead title="Home" />
+  //     {/* <main className={styles.main}>Home</main> */}
+  //   </>
+  // );
+}
+
+Home.getLayout = function getLayout(page: ReactElement) {
   return (
-    <>
-      <Components.PageHead title="Home" />
-      {/* <main className={styles.main}>Home</main> */}
-    </>
+    <Components.AuthenticationLayout>{page}</Components.AuthenticationLayout>
   );
+};
+
+export async function getServerSideProps() {
+  return { props: { authenticationRoute: true } };
 }
