e9fcea933cf28c7963f7bd7a6f33251c101ae604
Sekkacchi <sekkacchi@gmail.com>
2023-10-02 08:33:05 -0400

    Landing page now redirects to login page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import * as Components from "@/components";
+import * as Constants from "@/constants";
 
 import styles from "@/styles/home.module.scss";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(Constants.ClientRoutes.LOGIN, undefined, { shallow: true });
+  }, []);
+
   return (
     <>
       <Components.PageHead title="Home" />
